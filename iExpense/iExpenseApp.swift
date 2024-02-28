36283442eb548458a660df13e629c88e1eb48aa0
Jaskirat Singh <jaskirat1999@gmail.com>
2024-02-27 21:10:16 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jaskirat Singh on 2024-02-27.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
